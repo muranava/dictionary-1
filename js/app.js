63af3979f7f2a380a83d372d598613c8a1bab3bc
Radu Cojocaru <rcojocaru@gmail.com>
2011-05-23 07:30:20 +0100

    Added first quizz.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
-$(document).ready(function(){
-  console.log('ready');
-});
+var App = function(){
+  var dictionary;
+  var mainDivIDSelector;
+  
+  return {
+    
+    // Main entry point in to the app
+    run: function(dictionaryIn, mainDivIDIn) {
+      dictionary = dictionaryIn;
+      mainDivIDSelector = '#'+mainDivIDIn;
+      
+      App.showNextEntry();
+    },
+    
+    // Extract random entry and display it
+    showNextEntry: function() {
+      var entry = App.getRandomEntry();
+      App.display(entry);
+    },
+    
+    // 
+    display: function(entry) {
+      $(mainDivIDSelector).append(entry.from + " <input type='text' /> <input type='button' value='show' onclick=\"$('#translation').show();\" /> <span id='translation' style='display:none;'>" + entry.to + "</span>");
+    },
+    
+    // Get random entry from the dictionary
+    getRandomEntry: function() {
+      var entryIndex = App.randomFromTo(0, dictionary.entries.length);
+      return dictionary.entries[entryIndex];
+    },
+
+    // Generate random number between min and max
+    randomFromTo: function(from, to) {
+      return Math.floor(Math.random() * (to - from + 1) + from);
+    }
+  }
+}();
